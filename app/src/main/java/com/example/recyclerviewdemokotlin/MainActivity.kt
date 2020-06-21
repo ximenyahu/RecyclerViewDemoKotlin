348e35560eea1736e6f742a512ab649c7d0a2870
Simon <349283970@qq.com>
2020-06-21 21:04:56 +0800

    添加搜索功能

--- app/src/main/java/com/example/recyclerviewdemokotlin/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewdemokotlin/MainActivity.kt
@@ -4,40 +4,70 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    val mArrayList = ArrayList<Model>()
+    val mDisplayList = ArrayList<Model>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val arrayList = ArrayList<Model>()
-        arrayList.add(Model("Tim Duncan", "This is Tim Duncan", R.drawable.timduncan))
-        arrayList.add(Model("Tony Parker", "This is Tony Parker", R.drawable.tonyparker))
-        arrayList.add(Model("Manu Ginobili", "This is Manu Ginobili", R.drawable.manu_ginobili))
-        arrayList.add(Model("Kawhi Leonard", "This is Kawhi Leonard", R.drawable.kawhi_leonard))
-        arrayList.add(Model("Popovich", "This is Popovich", R.drawable.popovich))
-        arrayList.add(Model("Boris Diaw", "This is Boris Diaw", R.drawable.boris_diaw))
-        arrayList.add(Model("Patty Mills", "This is Patty Mills", R.drawable.patty_mills))
-        arrayList.add(Model("Tiago Splitter", "This is Tiago Splitter", R.drawable.tiago_splitter))
-        arrayList.add(
+        mArrayList.add(Model("Tim Duncan", "This is Tim Duncan", R.drawable.timduncan))
+        mArrayList.add(Model("Tony Parker", "This is Tony Parker", R.drawable.tonyparker))
+        mArrayList.add(Model("Manu Ginobili", "This is Manu Ginobili", R.drawable.manu_ginobili))
+        mArrayList.add(Model("Kawhi Leonard", "This is Kawhi Leonard", R.drawable.kawhi_leonard))
+        mArrayList.add(Model("Popovich", "This is Popovich", R.drawable.popovich))
+        mArrayList.add(Model("Boris Diaw", "This is Boris Diaw", R.drawable.boris_diaw))
+        mArrayList.add(Model("Patty Mills", "This is Patty Mills", R.drawable.patty_mills))
+        mArrayList.add(Model("Tiago Splitter", "This is Tiago Splitter", R.drawable.tiago_splitter))
+        mArrayList.add(
             Model(
                 "Stephen Jackson",
                 "This is Stephen Jackson",
                 R.drawable.stephen_jackson
             )
         )
-        arrayList.add(Model("Matty Bonner", "This is Matty Bonner", R.drawable.matt_bonner))
-        arrayList.add(Model("Nando De Colo", "This is Nando De Colo", R.drawable.nando_de_colo))
-        arrayList.add(Model("Cory Joseph", "This is Cory Joseph", R.drawable.cory_joseph))
-
-        val adapter = MyAdapter(this, arrayList)
+        mArrayList.add(Model("Matty Bonner", "This is Matty Bonner", R.drawable.matt_bonner))
+        mArrayList.add(Model("Nando De Colo", "This is Nando De Colo", R.drawable.nando_de_colo))
+        mArrayList.add(Model("Cory Joseph", "This is Cory Joseph", R.drawable.cory_joseph))
+        mDisplayList.addAll(mArrayList)
+        val adapter = MyAdapter(this, mDisplayList)
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
+        val item = menu!!.findItem(R.id.search)
+        if (item != null) {
+            val searchView: SearchView = item.actionView as SearchView
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    mDisplayList.clear()
+                    if (newText!!.isNotEmpty()) {
+                        val search = newText.toLowerCase(Locale.getDefault())
+                        mArrayList.forEach {
+                            if (it.title.toLowerCase(Locale.getDefault()).contains(search)) {
+                                mDisplayList.add(it)
+                            }
+                        }
+                    } else {
+                        mDisplayList.addAll(mArrayList)
+                    }
+                    recycler_view.adapter!!.notifyDataSetChanged()
+                    return true
+                }
+
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return true
+                }
+            })
+        }
         return true
     }
 
